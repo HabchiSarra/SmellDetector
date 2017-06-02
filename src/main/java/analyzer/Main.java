444d9c6d3e6a2bff9c1d25ee87f66294f2f65dd2
HABCHI Sarra <sarra.habchi@inria.fr>
2017-06-02 16:14:33 +0200

    refactor: update project structure

--- src/main/java/analyzer/Main.java
+++ src/main/java/analyzer/Main.java
@@ -17,42 +17,60 @@ public class Main {
 
 
     public static void main(String[] args) {
-        ArgumentParser parser = ArgumentParsers.newArgumentParser("paprika");
-        Subparsers subparsers = parser.addSubparsers().dest("sub_command");
-        Subparser analyseParser = subparsers.addParser("analyse").help("Analyse an app");
-        analyseParser.addArgument("folder").help("Path of the code source folder");
-        analyseParser.addArgument("-a", "--androidJar").required(true).help("Path to android platform jar");
-        analyseParser.addArgument("-db", "--database").required(true).help("Path to neo4J Database folder");
-        analyseParser.addArgument("-n", "--name").required(true).help("Name of the application");
-        analyseParser.addArgument("-p", "--package").required(false).help("Application main package");
-        analyseParser.addArgument("-k", "--key").required(true).help("sha256 of the apk used as identifier");
-        analyseParser.addArgument("-d", "--dependencies").required(true).help("Path to dependencies");
-        analyseParser.addArgument("-l", "--libs").help("List of the external libs used by the apps (separated by :)");
-        analyseParser.addArgument("-v", "--version").setDefault("").help("Version of the apps");
+        testRun();
+//        ArgumentParser parser = ArgumentParsers.newArgumentParser("paprika");
+//        Subparsers subparsers = parser.addSubparsers().dest("sub_command");
+//        Subparser analyseParser = subparsers.addParser("analyse").help("Analyse an app");
+//        analyseParser.addArgument("folder").help("Path of the code source folder");
+//        analyseParser.addArgument("-a", "--androidJar").required(true).help("Path to android platform jar");
+//        analyseParser.addArgument("-db", "--database").required(true).help("Path to neo4J Database folder");
+//        analyseParser.addArgument("-n", "--name").required(true).help("Name of the application");
+//        analyseParser.addArgument("-p", "--package").required(false).help("Application main package");
+//        analyseParser.addArgument("-k", "--key").required(true).help("sha256 of the apk used as identifier");
+//        analyseParser.addArgument("-d", "--dependencies").required(true).help("Path to dependencies");
+//        analyseParser.addArgument("-l", "--libs").help("List of the external libs used by the apps (separated by :)");
+//        analyseParser.addArgument("-v", "--version").setDefault("").help("Version of the apps");
+//
+//        Subparser queryParser = subparsers.addParser("query").help("Query the database");
+//        queryParser.addArgument("-db", "--database").required(true).help("Path to neo4J Database folder");
+//        queryParser.addArgument("-r", "--request").help("Request to execute");
+//        queryParser.addArgument("-c", "--csv").help("path to register csv files").setDefault("");
+//        queryParser.addArgument("-dk", "--delKey").help("key to delete");
+//        queryParser.addArgument("-dp", "--delPackage").help("Package of the applications to delete");
+//        queryParser.addArgument("-d", "--details").type(Boolean.class).setDefault(false).help("Show the concerned entity in the results");
+//
+//        try {
+//            Namespace res = parser.parseArgs(args);
+//            if(res.getString("sub_command").equals("analyse")){
+//                runAnalysis(res);
+//            }
+//            else if(res.getString("sub_command").equals("query")){
+//                queryMode(res);
+//            }
+//        } catch (ArgumentParserException e) {
+//            analyseParser.handleError(e);
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-        Subparser queryParser = subparsers.addParser("query").help("Query the database");
-        queryParser.addArgument("-db", "--database").required(true).help("Path to neo4J Database folder");
-        queryParser.addArgument("-r", "--request").help("Request to execute");
-        queryParser.addArgument("-c", "--csv").help("path to register csv files").setDefault("");
-        queryParser.addArgument("-dk", "--delKey").help("key to delete");
-        queryParser.addArgument("-dp", "--delPackage").help("Package of the applications to delete");
-        queryParser.addArgument("-d", "--details").type(Boolean.class).setDefault(false).help("Show the concerned entity in the results");
 
-        try {
-            Namespace res = parser.parseArgs(args);
-            if(res.getString("sub_command").equals("analyse")){
-                runAnalysis(res);
-            }
-            else if(res.getString("sub_command").equals("query")){
-                queryMode(res);
-            }
-        } catch (ArgumentParserException e) {
-            analyseParser.handleError(e);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+    }
 
+    public static void testRun() {
+        String path = "/home/sarra/Android-Projects/DuckDuckGo-Recursive/Android/app/src/main/java/com/duckduckgo/mobile/android";
+        String name = "DuckDuckGo";
+        String key = "DuckDuckGo";
+        String sdkPath = "/home/sarra/Android/Sdk/platforms/android-23/android.jar";
+        String jarsPath = "/home/sarra/Android-Projects/DuckDuckGo-Recursive/Android/app/runtime";
+        MainProcessor mainProcessor = new MainProcessor(name, key,key, path, sdkPath, jarsPath);
+        mainProcessor.process();
+        GraphCreator graphCreator = new GraphCreator(MainProcessor.currentApp);
+        graphCreator.createClassHierarchy();
+        graphCreator.createCallGraph();
+        MetricsCalculator.calculateAppMetrics(MainProcessor.currentApp);
+        ModelToGraph modelToGraph=new ModelToGraph("/home/sarra/Paprika-BDD/databases/graph.db");
+        modelToGraph.insertApp(MainProcessor.currentApp);
 
     }
 
@@ -252,84 +270,7 @@ public class Main {
 
 
 
-    public static void showModel(PaprikaApp app) {
-        System.out.println("App: " + app.getName());
-        for (PaprikaClass paprikaClass : app.getPaprikaClasses()) {
-            System.out.println("---------------------------------------------------------------------------------------------");
-            System.out.println(" Class : " + paprikaClass.getName());
-            System.out.println(" Visibility : " + paprikaClass.getModifier().name());
-            System.out.println(" isActivity : " + paprikaClass.isActivity());
-            System.out.println(" isInterface : " + paprikaClass.isInterface());
-            System.out.println(" isStatic : " + paprikaClass.isStatic());
-            System.out.println(" isApplication : " + paprikaClass.isApplication());
-            System.out.println(" isBroadcastReceiver : " + paprikaClass.isBroadcastReceiver());
-            System.out.println(" isContentProvider : " + paprikaClass.isContentProvider());
-            System.out.println(" isView : " + paprikaClass.isView());
-            System.out.println(" isService : " + paprikaClass.isService());
-            System.out.println(" isAsyncTask : " + paprikaClass.isAsyncTask());
-            System.out.println(" isInnerClass : " + paprikaClass.isInnerClass());
-            for (PaprikaMethod paprikaMethod : paprikaClass.getPaprikaMethods()) {
-                showMethod(paprikaMethod);
-            }
-            for (PaprikaVariable paprikaVariable : paprikaClass.getPaprikaVariables()) {
-                showVariable(paprikaVariable);
-            }
-            if (paprikaClass.getParent() != null) {
-                System.out.println(" Inherits of internal class: " + paprikaClass.getParent().getName());
-            } else if (paprikaClass.getParentName() != null) {
-                System.out.println(" Inherits of external class: " + paprikaClass.getParentName());
-            }
-            System.out.println("---------------------------------------------------------------------------------------------");
 
-        }
-    }
-
-    public static void showMethod(PaprikaMethod paprikaMethod) {
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        System.out.println(" Method : " + paprikaMethod.getName());
-        System.out.println(" Return type : " + paprikaMethod.getReturnType());
-        System.out.println(" Visbility : " + paprikaMethod.getModifier().name());
-        System.out.println(" isGetter : " + paprikaMethod.isGetter() + " isSetter : " + paprikaMethod.isSetter() + " isConstructor : " + paprikaMethod.isConstructor());
-        for (PaprikaArgument paprikaArgument : paprikaMethod.getArguments()) {
-            showArgument(paprikaArgument);
-        }
-        System.out.println("Calls +++++ ");
-        for (Entity entity : paprikaMethod.getCalledMethods()) {
-            if (entity instanceof PaprikaMethod) {
-                System.out.println(" Méthode : " + entity.getName() + " Classe "
-                        + ((PaprikaMethod) entity).getPaprikaClass().getName());
-
-            } else {
-                System.out.println(" Méthode externe: " + entity.getName() + " Classe "
-                        + ((PaprikaExternalMethod) entity).getPaprikaExternalClass().getName());
-
-            }
-
-        }
-
-        System.out.println("Uses +++++++");
-        for (PaprikaVariable paprikaVariable : paprikaMethod.getUsedVariables()) {
-            System.out.println(" Variable : " + paprikaVariable.getName() + " Classe : " + paprikaVariable.getPaprikaClass());
-        }
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-
-    }
-
-    public static void showArgument(PaprikaArgument paprikaArgument) {
-        System.out.println(" Argument : " + paprikaArgument.getName());
-        System.out.println(" position : " + paprikaArgument.getPosition());
-    }
-
-    public static void showVariable(PaprikaVariable paprikaVariable) {
-        System.out.println("*********************************************************************************************");
-        System.out.println(" Variable : " + paprikaVariable.getName());
-        System.out.println(" Type : " + paprikaVariable.getType());
-        System.out.println(" Visibility : " + paprikaVariable.getModifier().name());
-        System.out.println("*********************************************************************************************");
-
-    }
-
-    //public static void show
 
 
 }
