db9be4aa94dad50de43f5c69b208e67eb84e78ce
Antoine Veuiller <aveuiller@gmail.com>
2017-07-25 15:44:27 +0200

    fix: correctly output commit_number for UCS smell

--- src/main/java/neo4j/IGSQuery.java
+++ src/main/java/neo4j/IGSQuery.java
@@ -40,7 +40,8 @@ public class IGSQuery extends Query {
     @Override
     public void execute(boolean details) throws CypherException, IOException {
         try (Transaction ignored = graphDatabaseService.beginTx()) {
-            String query = "MATCH (a:App)-[:APP_OWNS_CLASS]->(cl:Class)-[:CLASS_OWNS_METHOD]->(m1:Method)-[:CALLS]->(m2:Method) WHERE (m2.is_setter OR m2.is_getter) AND (cl)-[:CLASS_OWNS_METHOD]->(m2) SET a.has_IGS=true RETURN a.commit_number as commit_number, m1.app_key as key";
+            String query = "MATCH (a:App)-[:APP_OWNS_CLASS]->(cl:Class)-[:CLASS_OWNS_METHOD]->(m1:Method)-[:CALLS]->(m2:Method) WHERE (m2.is_setter OR m2.is_getter) AND (cl)-[:CLASS_OWNS_METHOD]->(m2) SET a.has_IGS=true " +
+                    "RETURN a.commit_number as commit_number, m1.app_key as key";
             if(details){
                 query += ",m1.full_name + m2.full_name as instance, a.commit_status as commit_status ";
             }else{
