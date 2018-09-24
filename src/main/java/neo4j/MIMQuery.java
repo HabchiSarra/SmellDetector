1464fbfc77e1073c3176d37d1309983745bcf0f1
Antoine Veuiller <aveuiller@online.net>
2018-09-24 20:22:48 +0200

    fix(MIM): exclude interface methods

--- src/main/java/neo4j/MIMQuery.java
+++ src/main/java/neo4j/MIMQuery.java
@@ -39,7 +39,15 @@ public class MIMQuery extends Query {
 
     @Override
     protected String getQuery(boolean details) {
-        String query = "MATCH (a:App)-[:APP_OWNS_CLASS]->(cl:Class)-[:CLASS_OWNS_METHOD]->(m1:Method) WHERE m1.number_of_callers>0 AND NOT exists(m1.is_static) AND NOT exists(m1.is_override) AND NOT (m1)-[:USES]->(:Variable) AND NOT (m1)-[:CALLS]->(:ExternalMethod) AND NOT (m1)-[:CALLS]->(:Method) AND NOT exists(m1.is_init) " +
+        String query = "MATCH (a:App)-[:APP_OWNS_CLASS]->(cl:Class)-[:CLASS_OWNS_METHOD]->(m1:Method) " +
+                "WHERE m1.number_of_callers>0 " +
+                "AND NOT exists(m1.is_static) " +
+                "AND NOT exists(m1.is_override) " +
+                "AND NOT (m1)-[:USES]->(:Variable) " +
+                "AND NOT (m1)-[:CALLS]->(:ExternalMethod) " +
+                "AND NOT (m1)-[:CALLS]->(:Method) " +
+                "AND NOT exists(m1.is_init) " +
+                "AND NOT exists(cl.is_interface) w" +
                 "RETURN DISTINCT a.commit_number as commit_number, m1.app_key as key, cl.file_path as file_path";
         if (details) {
             query += ",m1.full_name as instance";
