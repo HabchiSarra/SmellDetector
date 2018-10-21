0cbb8c2adacd4de9cc5e63b0cc5783c11ca35af4
Antoine Veuiller <aveuiller@online.net>
2018-10-21 19:50:04 +0200

    feat: enhance CommitSizeQuery

--- src/main/java/fr/inria/sniffer/detector/neo4j/CommitSizeQuery.java
+++ src/main/java/fr/inria/sniffer/detector/neo4j/CommitSizeQuery.java
@@ -13,8 +13,19 @@ public class CommitSizeQuery extends Query {
 
     @Override
     protected String getQuery(boolean details) {
-        String query = "match(a:App) return a.app_key as sha1, " +
-                "a.number_of_classes as number_of_classes,  a.number_of_methods as number_of_methods";
+        String query = "match(a:App) RETURN " +
+                "a.app_key as sha1, " +
+                "a.number_of_classes as number_of_classes, " +
+                "a.number_of_methods as number_of_methods, " +
+                "a.number_of_interfaces as number_of_interfaces, " +
+                "a.number_of_broadcast_receivers as number_of_broadcast_receivers, " +
+                "a.number_of_services as number_of_services, " +
+                "a.number_of_content_providers as number_of_content_providers, " +
+                "a.number_of_activities as number_of_activities, " +
+                "a.number_of_variables as number_of_variables, " +
+                "a.number_of_inner_classes as number_of_inner_classes, " +
+                "a.number_of_async_tasks as number_of_async_tasks, " +
+                "a.number_of_views as number_of_views";
         return query;
     }
 }
